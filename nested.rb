c1d5fae1260197931830e6ae27e0516909946c93
Valentin Placido <valentinplacido25@gmail.com>
2020-01-16 18:29:41 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -21,8 +21,7 @@ def hopper
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
+
 	programmer_hash = 
  		{
         :grace_hopper => {
